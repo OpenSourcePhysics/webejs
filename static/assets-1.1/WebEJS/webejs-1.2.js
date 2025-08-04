7b350facb1e700fc116b5f41c6d5635b7a28fe8f
PacoEsquembre <fem@um.es>
2025-08-04 20:03:03 +0200

    minor

--- static/assets-1.1/WebEJS/webejs-1.2.js
+++ static/assets-1.1/WebEJS/webejs-1.2.js
@@ -2154,14 +2154,7 @@ var WebEJS_GUI = WebEJS_GUI || {};
 
 WebEJS_GUI.comm = function(sessionID, mServerSubroute) {
 	var self = {};
-	const TEXT_TOOLS = WebEJS_TOOLS.textTools();
-
-  console.log ("====== Server subrout = "+mServerSubroute)
-	if (!mServerSubroute) mServerSubroute = '';
-
-  console.log ("====== Session ID = "+sessionID)
-  console.log ("====== Server subrout = "+mServerSubroute)
-  
+	const TEXT_TOOLS = WebEJS_TOOLS.textTools();  
 
 	// ----------------------------
 	// Ajax functions
@@ -2189,7 +2182,6 @@ WebEJS_GUI.comm = function(sessionID, mServerSubroute) {
 
 	function ajaxPost(url, message, onSuccess, onErrorMessage, onErrorData, onErrorListener) {
     message['session_id'] = sessionID;
-		console.log ("====== COMM CSRFToken = "+sMainCsrfToken)
 		$.ajax({
 			type: 'POST', 
 			url: mServerSubroute + url,
@@ -2541,33 +2533,7 @@ WebEJS_GUI.comm = function(sessionID, mServerSubroute) {
 			'User file command failed:'+arguments);
 	}
 
-	/*
-  $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-      function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-          var cookies = document.cookie.split(';');
-          for (var i = 0; i < cookies.length; i++) {
-            var cookie = $.trim(cookies[i]);
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-            }
-          }
-        }
-        return cookieValue;
-      }
-      if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-      }
-			console.log("CRSF token = <"+getCookie('csrftoken')+">");
-    },
-		xhrFields: {
-			withCredentials: true // Send cookies if the origin is different
-		}
-  });
-*/
+
   return self;
 }
 /*
